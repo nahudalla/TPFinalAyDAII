fec07112ff1920bc5076c8d8a0f9f8896e6e36d1
Nahuel Dallacamina <nahudalla@gmail.com>
2019-01-22 23:46:50 -0300

    Added message when page is ready

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -2,3 +2,6 @@ import './setup/statusBarLoggerSetup.js';
 import './setup/toolsSetup.js';
 import './setup/canvasSetup.js';
 import './setup/fileDropSetup.js';
+import log from './logger.js';
+
+log(log.FLAGS.INFO, "Listo.");

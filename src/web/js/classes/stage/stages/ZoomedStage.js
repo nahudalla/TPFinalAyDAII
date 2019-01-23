9c94091c5245bb743b152dff56d570da9e4bf51e
Nahuel Dallacamina <nahudalla@gmail.com>
2019-01-22 23:55:35 -0300

    Add point support implemented + scroll delegated to super class

--- src/web/js/classes/stage/stages/ZoomedStage.js
+++ src/web/js/classes/stage/stages/ZoomedStage.js
@@ -1,15 +1,27 @@
 import StageBase from './StageBase.js';
+import Point from '../../Point.js';
 
 export default class ZoomedStage extends StageBase {
   constructor(project, stage) {
     super(project, stage);
 
+    super._centerOriginInView();
     super._enableZoom();
+    super._enableScroll();
 
-    stage.scrollEvent.subscribe(offset => {
-      const view = project.view;
-      const viewCenter = view.projectToView(view.center);
-      view.center = view.viewToProject(viewCenter.subtract(offset));
-    });
+    this._layer = project.activeLayer;
+  }
+
+  addPoint(point) {
+    if(!(point instanceof Point)) throw new Error('Invalid Point type!');
+
+    this._pushStyle(this.stage.settings.point_style);
+
+    this._layer.addChild(new this.stage.Circle({
+      center: (new this.stage.Point(point.x, -point.y)).multiply(this.stage.settings.grid_size),
+      radius: this.stage.settings.point_radius
+    }));
+
+    this._popStyle();
   }
 }

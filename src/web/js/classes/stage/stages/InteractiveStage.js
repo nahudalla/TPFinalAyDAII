856a308a123d2528c249cf277f9137572e1cd922
Nahuel Dallacamina <nahudalla@gmail.com>
2019-01-23 23:22:51 -0300

    New StageWithPointsBase superclass + new grid events in InteractiveStage

--- src/web/js/classes/stage/stages/InteractiveStage.js
+++ src/web/js/classes/stage/stages/InteractiveStage.js
@@ -1,24 +1,49 @@
-import StageBase from './StageBase.js';
 import Observable from '../../Observable.js';
+import Point from '../../Point.js';
+import StageWithPointsBase from './StageWithPointsBase.js';
 
-export default class InteractiveStage extends StageBase {
+export default class InteractiveStage extends StageWithPointsBase {
   constructor(project, stage) {
     super(project, stage);
 
     super._centerOriginInView();
+    super._enableResetCenterOnResize();
     super._enableZoom();
     super._enableScroll();
 
     this._clickEvent = new Observable();
+    this._downEvent = new Observable();
     this._dragEvent = new Observable();
+    this._upEvent = new Observable();
+    this._gridPositionEvent = new Observable();
+    this._gridOverEvent = new Observable();
+    this._gridOutEvent = new Observable();
+
+    this._lastGridPositionPoint = null;
 
     this._setupZoom();
     this._setupClickEvent();
     this._setupDragEvent();
+    this._setupGridEvents();
   }
+
+  _getPointStyle() { return this.stage.settings.interactive_point_style; }
 
   get clickEvent() { return this._clickEvent; }
   get dragEvent() { return this._dragEvent; }
+  get downEvent() { return this._downEvent; }
+  get upEvent() { return this._upEvent; }
+  get gridPositionEvent() { return this._gridPositionEvent; }
+  get gridOverEvent() { return this._gridOverEvent; }
+  get gridOutEvent() { return this._gridOutEvent; }
+
+  setCursor(cursor) {
+    this.view.element.style.cursor = cursor;
+  }
+
+  resetCursor() {
+    this.setCursor(this.stage.settings.interactive_default_cursor);
+  }
 
   _setupZoom() {
     const elem = this.project.view.element;
@@ -36,8 +61,10 @@ export default class InteractiveStage extends StageBase {
   _setupClickEvent() {
     const element = this.project.view.element;
     element.addEventListener('click', event => {
+      const position = new this.stage.Point(calculateMousePos(element, event));
+
       this._clickEvent.emit({
-        position: new this.stage.Point(calculateMousePos(element, event)),
+        position, gridPosition: this._calculateGridPosition(position),
         event
       });
     });
@@ -45,6 +72,7 @@ export default class InteractiveStage extends StageBase {
 
   _setupDragEvent() {
     const element = this.project.view.element;
+    const _this = this;
 
     let lastPosition, isDrag;
 
@@ -53,6 +81,10 @@ export default class InteractiveStage extends StageBase {
 
     element.addEventListener('mousedown', e => {
       lastPosition = new this.stage.Point(calculateMousePos(element, e));
+      this._downEvent.emit({position:lastPosition, event:e});
+
+      if(e.defaultPrevented) return;
+
       isDrag = false;
       window.addEventListener('mousemove', onMouseMoveBound);
       window.addEventListener('mouseup', onMouseUpBound);
@@ -67,7 +99,7 @@ export default class InteractiveStage extends StageBase {
       const previous_position = lastPosition;
       lastPosition = position;
 
-      this._dragEvent.emit({
+      _this._dragEvent.emit({
         offset: position.subtract(previous_position),
         position, previous_position, event
       });
@@ -80,7 +112,42 @@ export default class InteractiveStage extends StageBase {
       }
       window.removeEventListener('mousemove', onMouseMoveBound);
       window.removeEventListener('mouseup', onMouseUpBound);
+      _this._upEvent.emit({
+        position: calculateMousePos(element, e),
+        isDrag, event: e
+      });
     }
+  }
+
+  _setupGridEvents() {
+    const element = this.project.view.element;
+    element.addEventListener('mouseout', event => {
+      this._gridOutEvent.emit(event);
+    });
+    element.addEventListener('mousemove', event => {
+      const positionCount = this._gridPositionEvent.listenerCount;
+      const overCount = this._gridOverEvent.listenerCount;
+
+      if(positionCount === 0 && overCount === 0) return;
+
+      const position = this._calculateGridPosition(calculateMousePos(element, event));
+
+      if(overCount !== 0) this._gridOverEvent.emit({position, event});
+
+      if( positionCount === 0
+          || (this._lastGridPositionPoint && this._lastGridPositionPoint.equals(position))
+      ) { return; }
+
+      this._lastGridPositionPoint = position;
+
+      this._gridPositionEvent.emit({position, event});
+    });
+  }
+
+  _calculateGridPosition(viewPosition) {
+    const pos = this.view.viewToProject(viewPosition)
+      .divide(this.stage.settings.grid_size);
+    return new Point(Math.round(pos.x), Math.round(-pos.y));
   }
 }
 
--- src/web/js/classes/stage/stages/ZoomedStage.js
+++ src/web/js/classes/stage/stages/ZoomedStage.js
@@ -1,27 +1,14 @@
-import StageBase from './StageBase.js';
-import Point from '../../Point.js';
+import StageWithPointsBase from './StageWithPointsBase.js';
 
-export default class ZoomedStage extends StageBase {
+export default class ZoomedStage extends StageWithPointsBase {
   constructor(project, stage) {
     super(project, stage);
 
     super._centerOriginInView();
+    super._enableResetCenterOnResize();
     super._enableZoom();
     super._enableScroll();
-
-    this._layer = project.activeLayer;
   }
 
-  addPoint(point) {
-    if(!(point instanceof Point)) throw new Error('Invalid Point type!');
-
-    this._pushStyle(this.stage.settings.point_style);
-
-    this._layer.addChild(new this.stage.Circle({
-      center: (new this.stage.Point(point.x, -point.y)).multiply(this.stage.settings.grid_size),
-      radius: this.stage.settings.point_radius
-    }));
-
-    this._popStyle();
-  }
+  _getPointStyle() { return this.stage.settings.point_style; }
 }

b5e0268b4cf47a0702192a62a2edc8c4c1613c77
Nahuel Dallacamina <nahudalla@gmail.com>
2019-02-21 01:08:46 -0300

    Test code removed

--- src/web/js/ui/UISetup.js
+++ src/web/js/ui/UISetup.js
@@ -1,6 +1,5 @@
 import { Context } from '../classes/Context.js';
 import Point from '../classes/Point.js';
-import Arrow from '../classes/Arrow.js';
 
 const AXES_CANVAS_ID = "canvas_axes";
 const ZOOMED_CANVAS_ID = "canvas_zoomed";
@@ -27,26 +26,3 @@ context.pointsList.add(new Point(3,-3));
 context.pointsList.add(new Point(-3,3));
 context.pointsList.add(new Point(-3,-3));
 context.pointsList.add(new Point(-1,-1));
-
-context.stage.normal_back.addObject(new Arrow(
-  new Point(-2, -2),
-  new Point(3, 3)
-));
-
-context.stage.normal_front.addObject(new Arrow(
-  new Point(2, 2),
-  new Point(-3, -3)
-));
-
-context.algorithms.grahamScan.enable();
-context.algorithms.jarvisMarch.enable();
-context.algorithms.closestPairOfPoints.enable();
-
-const setEvents = runner => {
-  runner.progressEvent.subscribe((...args) => console.log(...args));
-  runner.doneEvent.subscribe((...args) => console.log(...args));
-  runner.errorEvent.subscribe((...args) => console.log(...args));
-};
-
-const runner = context.algorithms.run();
-setEvents(runner);

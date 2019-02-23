aaee9d9b63cdc4e1e7c52b73843657261901072f
Nahuel Dallacamina <nahudalla@gmail.com>
2019-02-23 19:25:25 -0300

    Full application distributable implementation
     - Electron app implementation
     - CHANGELOG file added
     - NPM scripts added to generate packages for the three major platforms
     - Custom script to generate distributable zips based on each platform's
       package

---
+++ generateDistributables.js
@@ -0,0 +1,184 @@
+// This file generates three distributable .zip files from the packaged versions of the app,
+// a release.json file describing the current release, a download page for users to select
+// a specific platform distributable and a configuration file to upload everything to Firebase
+
+const zipper = require('zip-a-folder');
+const FS = require('fs');
+const rimraf = require('rimraf');
+
+const OUTPUT_DIRECTORY = "./dist";
+const CONTENT_DIRECTORY = `${OUTPUT_DIRECTORY}/public`;
+const PACKAGES_DIRECTORY = "./packaged";
+const BASE_URL = "https://tpaydaii.nahueldallacamina.com.ar";
+const FIREBASE_CONFIG_FILENAME = "firebase.json";
+const RELEASE_JSON_FILENAME = "release.json";
+const CHANGELOG_FILE = "./CHANGELOG";
+const DOWNLOAD_PAGE_FILENAME = "download.html";
+const PACKAGE_JSON_FILE = "./package.json";
+
+const PLATFORMS = [
+  {
+    platform: 'darwin',
+    displayName: 'Mac OS',
+    distributableName: 'macOS'
+  },
+  {
+    platform: 'linux',
+    displayName: 'Linux',
+    distributableName: 'linux'
+  },
+  {
+    platform: 'win32',
+    displayName: 'Windows',
+    distributableName: 'win32'
+  }
+];
+
+const FIREBASE_CONFIG = {
+  "hosting": {
+    "public": "public",
+    "ignore": [
+      "firebase.json",
+      "**/.*",
+      "**/node_modules/**"
+    ],
+    "headers": [ {
+      "source": "*",
+      "headers": [ {
+        "key": "Access-Control-Allow-Origin",
+        "value": "*"
+      } ]
+    } ]
+  }
+};
+
+void async function main() {
+  console.log('Cleaning up old distributable files...');
+  rimraf.sync(OUTPUT_DIRECTORY);
+
+  ensureDirectoryExists(OUTPUT_DIRECTORY);
+  ensureDirectoryExists(CONTENT_DIRECTORY);
+
+  console.log('Reading version information from "package.json"...');
+
+  const package_json = JSON.parse(FS.readFileSync(PACKAGE_JSON_FILE, "utf8"));
+  const version = package_json.version;
+
+  if(!version) {
+    console.error('No version field in package.json!');
+    return;
+  }
+
+  console.log(`Version is: ${version}`);
+
+  console.log('Reading changelog...');
+
+  const changelog = FS.readFileSync(CHANGELOG_FILE, "utf8");
+
+  if(!changelog) {
+    console.error('Changelog empty!');
+    return;
+  }
+
+  if(!changelog.includes(version)) {
+    console.error('Version number not found in changelog!');
+    return;
+  }
+
+  console.log('Generating distributables...');
+
+  await generateDistributables(version);
+
+  console.log(`Generating "${RELEASE_JSON_FILENAME}"...`);
+
+  FS.writeFileSync(
+    `${CONTENT_DIRECTORY}/${RELEASE_JSON_FILENAME}`,
+    JSON.stringify(generateReleaseJSON(version, changelog))
+  );
+
+  console.log('Generating download page...');
+
+  FS.writeFileSync(
+    `${CONTENT_DIRECTORY}/${DOWNLOAD_PAGE_FILENAME}`,
+    generateDownloadPage(version)
+  );
+
+  console.log('Writing Firebase configuration...');
+
+  FS.writeFileSync(
+    `${OUTPUT_DIRECTORY}/${FIREBASE_CONFIG_FILENAME}`,
+    JSON.stringify(FIREBASE_CONFIG)
+  );
+
+  console.log('Done');
+}();
+
+function generatePackagePath(platform) {
+  return `${PACKAGES_DIRECTORY}/tpfinalaydaii-${platform.platform}-x64/`;
+}
+
+function generateDistributableFilename(platform, version) {
+  return `TPFinalAyDAII_v${version}_${platform.distributableName}_x64.zip`;
+}
+
+function generateDownloadPage(version) {
+  let platforms = '';
+  PLATFORMS.forEach(platform => {
+    platforms += `<li>
+<b>${platform.displayName} (x64):</b>
+<a href="${BASE_URL}/${generateDistributableFilename(platform,version)}">${generateDistributableFilename(platform,version)}</a>
+</li>`;
+  });
+
+  return `
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8" />
+<title>TPFinalAyDAII - Descargar</title>
+</head>
+<body>
+<h1>TPFinalAyDAII - v${version}</h1>
+<p>Este trabajo fue hecho para final de las materias Análisis y Diseño de Algoritmos I y II, en la
+Universidad Nacional del Centro de la Provincia de Buenos Aires, Argentina. Desarrollado por Nahuel
+Dallacamina Ortea entre Noviembre de 2018 y Febrero de 2019.</p>
+<p>El objetivo fue realizar una herramienta que permita trabajar con algoritmos geométricos básicos
+y permita realizar una ejecución paso a paso de los mismos, con fines didácticos.</p>
+<p>Algunas funcionalidades propias de la interfaz gráfica aún no se encuentran finalizadas. La 
+herramienta disponible para descarga en esta página verificará periódicamente si hay nuevas versiones
+disponibles y mostrará un mensaje para que el usuario pueda ingresar a esta página nuevamente y 
+descargue la última versión disponible.</p>
+<p>Es posible acceder al código de fuente de esta herramienta en GitHub: <a href="https://github.com/nahudalla/TPFinalAyDAII">https://github.com/nahudalla/TPFinalAyDAII</a></p>
+<p>A continuación, descargue el archivo comprimido que corresponda para su plataforma. Una vez
+descargado, descomprímalo y ejecute el archivo "tpfinalaydaii", con la extensión que corresponda
+para su plataforma.</p>
+<p><b>La herramienta sólo se distribuye para la arquitectura x64.</b></p>
+<ul>${platforms}</ul>
+</body>
+</html>
+`;
+}
+
+async function generateDistributables(version) {
+  for(let platform of PLATFORMS) {
+    const path = generatePackagePath(platform);
+    if(!FS.existsSync(path)) {
+      console.error(`Skipping platform ${platform.displayName}, package not found.`);
+      return;
+    }
+
+    await zipper.zip(path, `${CONTENT_DIRECTORY}/${generateDistributableFilename(platform, version)}`);
+  }
+}
+
+function ensureDirectoryExists(directory) {
+  if (!FS.existsSync(directory)){
+    FS.mkdirSync(directory);
+  }
+}
+
+function generateReleaseJSON(version, changelog) {
+  return {
+    version, changelog, url: `${BASE_URL}/${DOWNLOAD_PAGE_FILENAME}`
+  };
+}
--- src/web/electron-startup.js
+++ src/web/electron-startup.js
@@ -1,35 +1,136 @@
-import { app, BrowserWindow } from 'electron';
+const { app, BrowserWindow, session, shell } = require('electron');
+const FS = require('fs');
+const finalhandler = require('finalhandler');
+const http = require('http');
+const serveStatic = require('serve-static');
+
+const URL = require('url').URL;
+
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-navigate', (event, navigationUrl) => {
+    const parsedUrl = new URL(navigationUrl);
+
+    if (parsedUrl.origin !== 'http://localhost') {
+      event.preventDefault()
+    }
+  });
+
+  contents.on('new-window', (event, navigationUrl) => {
+    // We'll ask the operating system
+    // to open this event's url in the default browser.
+    event.preventDefault();
+
+    if(navigationUrl.startsWith('https://')) {
+      shell.openExternal(navigationUrl);
+    }
+  });
+});
+
+// Load package.json
+let app_version = null;
+try {
+  const package_json = JSON.parse(FS.readFileSync(__dirname+'/package.json'));
+  if(package_json && package_json.version) {
+    app_version = package_json.version;
+  }
+}catch(e){
+  console.error(e);
+}
+
+if(app_version) console.info('App version: '+app_version);
+
+// Serve up . folder
+var serve = serveStatic(__dirname, {setHeaders:(res, path)=>{
+  if(path.endsWith('.wasm')) {
+    res.setHeader('Content-Type', 'application/wasm');
+  }
+}});
+
+// Create server
+const server = http.createServer( (req, res) => {
+  serve(req, res, finalhandler(req, res));
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow, errWindow;
 
-const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+const appReady = () => {
+  session
+    .fromPartition('some-partition')
+    .setPermissionRequestHandler((webContents, permission, callback) => {
+      // Denies the permissions request
+      return callback(false)
+    });
+
+  server.on('error',()=>{
+    if(errWindow) return;
+
+    errWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      nodeIntegration: false
+    });
+
+    errWindow.loadURL(`file://${__dirname}/appError.html`);
+
+    errWindow.on('closed', ()=>{
+      errWindow.close();
+    });
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  server.listen(()=>{
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      center: true,
+      autoHideMenuBar: true,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        devTools: false,
+        webaudio: false,
+        plugins: false
+      }
+    });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+    const addr = server.address();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+      callback({
+        responseHeaders: {
+          ...details.responseHeaders,
+          'Content-Security-Policy': [
+            `script-src 'unsafe-inline' http://localhost:${addr.port}`,
+            `style-src 'unsafe-inline' http://localhost:${addr.port}`,
+            `worker-src blob:http://localhost:${addr.port}`,
+            `default-src 'none'`].join(';')
+        }
+      })
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(`http://localhost:${addr.port}/` + (app_version?`?version=${app_version}`:''));
+
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+      mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+        server.close();
+      });
   });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', appReady);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -44,7 +145,7 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    appReady();
   }
 });
 

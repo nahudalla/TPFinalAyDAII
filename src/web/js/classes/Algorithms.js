1170fd08e4a3f081f5872b7ed62bed62318c9b72
Nahuel Dallacamina <nahudalla@gmail.com>
2019-02-24 12:59:18 -0300

    Improve code

--- src/web/js/classes/Algorithms.js
+++ src/web/js/classes/Algorithms.js
@@ -9,44 +9,33 @@ export default class Algorithms {
   constructor(context) {
     this._context = context;
 
-    this._grahamScan = new GrahamScan(context);
-    this._jarvisMarch = new JarvisMarch(context);
-    this._closestPairOfPoints = new ClosestPairOfPoints(context);
-    this._anySegmentsIntersect = new AnySegmentsIntersect(context);
+    const algorithms = {
+      grahamScan : new GrahamScan(context),
+      jarvisMarch : new JarvisMarch(context),
+      closestPairOfPoints : new ClosestPairOfPoints(context),
+      anySegmentsIntersect : new AnySegmentsIntersect(context)
+    };
 
     this._enabledAlgorithmsChangedEvent = new Observable();
-    const statusChanged = (algor, enabled)=>{
-      this._enabledAlgorithmsChangedEvent.emit(algor, enabled);
-    };
-    this._grahamScan.enabledStatusChangedEvent.subscribe(statusChanged.bind(this, this._grahamScan));
-    this._jarvisMarch.enabledStatusChangedEvent.subscribe(statusChanged.bind(this, this._jarvisMarch));
-    this._closestPairOfPoints.enabledStatusChangedEvent.subscribe(statusChanged.bind(this, this._closestPairOfPoints));
-    this._anySegmentsIntersect.enabledStatusChangedEvent.subscribe(statusChanged.bind(this, this._anySegmentsIntersect));
-
     this._algorithmsById = new Map();
 
-    this._algorithmsById.set(this._grahamScan.id, this._grahamScan);
-    this._algorithmsById.set(this._jarvisMarch.id, this._jarvisMarch);
-    this._algorithmsById.set(this._closestPairOfPoints.id, this._closestPairOfPoints);
-    this._algorithmsById.set(this._anySegmentsIntersect.id, this._anySegmentsIntersect);
+    Object.keys(algorithms).forEach(algorithm => {
+      const algor = algorithms[algorithm];
+      this[`_${algorithm}`] = algor;
+      algor.enabledStatusChangedEvent.subscribe(enabled=>{
+        this._enabledAlgorithmsChangedEvent.emit(algor, enabled);
+      });
+      this._algorithmsById.set(algor.id, algor);
+      Object.defineProperty(this, algorithm, {get: () => algor});
+    });
   }
   
   get enabledAlgorithmsChangedEvent() {
     return this._enabledAlgorithmsChangedEvent;
   }
 
-  get grahamScan() { return this._grahamScan; }
-  get jarvisMarch() { return this._jarvisMarch; }
-  get closestPairOfPoints() { return this._closestPairOfPoints; }
-  get anySegmentsIntersect() { return this._anySegmentsIntersect; }
-
   get listAll() {
-    return [
-      this._grahamScan,
-      this._jarvisMarch,
-      this._closestPairOfPoints,
-      this._anySegmentsIntersect
-    ];
+    return Array.from(this._algorithmsById.values());
   }
 
   get(algorId) {
